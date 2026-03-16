d7110ed571799a7d112f262ed1dd8f3cdb672e6a
Atyla da Silva <102058935+AtylaDaSilva@users.noreply.github.com>
2026-03-15 21:50:20 -0300

    Added best-audio-available.bat

---
+++ best-audio-available.bat
@@ -0,0 +1,26 @@
+::Disable echo
+@echo off
+
+::Variables
+set "YT_URL=%~1"
+
+if not defined YT_URL (
+    echo "Missing URL"
+    exit \b
+)
+
+
+set "FILE_PATH=C:\yt-dlp\out"
+set "FILE_NAME=%%(title)s.%%(ext)s"
+set FORMAT=ba
+set "FFMPEG_PATH=C:\ffmpeg\bin\ffmpeg.exe"
+
+::Echo params
+echo %YT_URL%
+echo %FILE_PATH%
+echo %FILE_NAME%
+echo %FORMAT%
+echo %FFMPEG_PATH%
+
+::Run command
+yt-dlp %YT_URL% -P %FILE_PATH% -o %FILE_NAME% -f %FORMAT% --no-playlist --ffmpeg-location %FFMPEG_PATH%
