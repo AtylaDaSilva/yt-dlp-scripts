5838328815dda535ebd28f6e1b807a9d8bbfbbda
Atyla da Silva <atylamendes97@gmail.com>
2026-03-16 19:29:36 -0300

    Settings file support

--- best-audio-available.bat
+++ best-audio-available.bat
@@ -6,37 +6,42 @@ echo ========== yt-dlp Scripts ==========
 echo ------- Best Audio Available -------
 echo ====================================
 
-::Variables
+::Read .ini file
+for /f "tokens=1,2 delims=^=" %%A in (yt-dlp-scripts.ini) do (
+    set "%%A=%%~B"
+)
+
+::Validations
 set ERRMSG=
 set "YT_URL=%1"
 
+::Validations - Missing URL
 if not defined YT_URL (
     set ERRMSG=Missing or invalid URL parameter
     goto end
 )
 
+::Validations - Unquoted URL
 set "YT_URL_UNQUOTED=%~1"
 
-if %YT_URL%==%YT_URL% (
+if %YT_URL%==%YT_URL_UNQUOTED% (
     set ERRMSG=URL parameter appears to be unquotted. Make sure the URL is wrapped in double quotes
     goto end
 )
 
-set "FILE_PATH=C:\yt-dlp\out"
-set "FILE_NAME=%%(title)s.%%(ext)s"
-set FORMAT=ba
-set "FFMPEG_PATH=C:\ffmpeg\bin\ffmpeg.exe"
+::File format - List of supported formats: https://github.com/yt-dlp/yt-dlp?tab=readme-ov-file#format-selection
+set FILE_FORMAT=ba
 
 ::Echo params
 echo %YT_URL%
 @REM echo %YT_URL_UNQUOTED% DEBUG
 echo %FILE_PATH%
 echo %FILE_NAME%
-echo %FORMAT%
+echo %FILE_FORMAT%
 echo %FFMPEG_PATH%
 
 ::Run command
-yt-dlp %YT_URL% -P %FILE_PATH% -o %FILE_NAME% -f %FORMAT% --no-playlist --ffmpeg-location %FFMPEG_PATH%
+yt-dlp %YT_URL% -P %FILE_PATH% -o %FILE_NAME% -f %FILE_FORMAT% --no-playlist --ffmpeg-location %FFMPEG_PATH%
 
 :end
 if defined ERRMSG echo %ERRMSG%
