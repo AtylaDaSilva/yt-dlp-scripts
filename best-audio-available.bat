590b776c1976a1cddd0ca489b413da0f8f7d9708
Atyla da Silva <atylamendes97@gmail.com>
2026-03-16 18:17:22 -0300

    Unquoted URL validation & better error handling

--- best-audio-available.bat
+++ best-audio-available.bat
@@ -7,13 +7,20 @@ echo ------- Best Audio Available -------
 echo ====================================
 
 ::Variables
-set "YT_URL=%~1"
+set ERRMSG=
+set "YT_URL=%1"
 
 if not defined YT_URL (
-    echo Missing URL parameter
-    exit \b
+    set ERRMSG=Missing or invalid URL parameter
+    goto end
 )
 
+set "YT_URL_UNQUOTED=%~1"
+
+if %YT_URL%==%YT_URL% (
+    set ERRMSG=URL parameter appears to be unquotted. Make sure the URL is wrapped in double quotes
+    goto end
+)
 
 set "FILE_PATH=C:\yt-dlp\out"
 set "FILE_NAME=%%(title)s.%%(ext)s"
@@ -22,10 +29,16 @@ set "FFMPEG_PATH=C:\ffmpeg\bin\ffmpeg.exe"
 
 ::Echo params
 echo %YT_URL%
+@REM echo %YT_URL_UNQUOTED% DEBUG
 echo %FILE_PATH%
 echo %FILE_NAME%
 echo %FORMAT%
 echo %FFMPEG_PATH%
 
 ::Run command
-yt-dlp %YT_URL% -P %FILE_PATH% -o %FILE_NAME% -f %FORMAT% --no-playlist --ffmpeg-location %FFMPEG_PATH%
+:yt-dlp %YT_URL% -P %FILE_PATH% -o %FILE_NAME% -f %FORMAT% --no-playlist --ffmpeg-location %FFMPEG_PATH%
+
+:end
+if defined ERRMSG echo %ERRMSG%
+echo Exiting...
+::exit \b
